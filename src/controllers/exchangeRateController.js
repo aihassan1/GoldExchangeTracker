1f7b8a075da1d4330702572cbfccffc72ee1c50a
aihassan1 <abdosruor57@gmail.com>
2024-08-26 08:24:30 +0300

    Fix error handling and response returning in getExchangeRateTimeframe method

--- src/controllers/exchangeRateController.js
+++ src/controllers/exchangeRateController.js
@@ -1,4 +1,7 @@
-import {getExchangeRate} from '../services/getExchangeRate.js';
+import {
+  getExchangeRate,
+  getExchangeRateTimeframe,
+} from '../services/getExchangeRate.js';
 class ExchangeRateController {
   static async exchangeRate(req, res) {
     try {
@@ -7,6 +10,26 @@ class ExchangeRateController {
     } catch (err) {
       console.log(err);
       res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+
+  static async getExchangeRateTimeframe(req, res) {
+    const start_date = req.query.start_date;
+    const end_date = req.query.end_date;
+    if (!start_date || !end_date) {
+      res.status(500).json({ error: 'Missing start_date or end_date' });
+    }
+
+    try {
+      const exchangeRates = await getExchangeRateTimeframe(
+        start_date,
+        end_date
+      );
+    } catch (err) {
+      res.status(500).json({
+        error: 'error fetching the exchange rate per time frame',
+        details: err.message,
+      });
     }
   }
 }
